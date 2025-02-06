e40b3dd320953de937750d473132585fd41e8bc3
Kaio <kaio.santiago.13@hotmail.com>
2025-02-06 08:54:27 -0300

    create model Code

--- model/code.go
+++ model/code.go
@@ -1,4 +1,14 @@
 package model
 
+import "time"
+
 type Code struct {
+	Id       int    `json:"id" gorm:"primaryKey"`
+	Tag      string `json:"tag"`
+	Script   string
+	CreateAt time.Time `json:"createAt" gorm:"column:create_at;default:CURRENT_TIMESTAMP"`
+}
+
+func (Code) TableName() string {
+	return "t_register_codes"
 }
